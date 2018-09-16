2c292cbb76f55252f7d51cfda8a4475b54a57fc0
qinpmc <850823973@qq.com>
2018-09-16 12:02:19 +0800

    add mime demo

--- ch1/http2/httpserver.js
+++ ch1/http2/httpserver.js
@@ -7,18 +7,21 @@ http.createServer(function(req,res){
     var urlObj = url.parse(req.url);
     var pathname = urlObj.pathname;
     console.log(pathname);
-    if( pathname=="/"){  //请求地址： http://localhost:8888
-        res.setHeader("Content-Type","text/html;charset=utf-8");
-        fs.readFile("index.html","utf-8",function(err,data){
-            res.write(data);
-            res.end();
-        })
-    }else{
-        res.setHeader("Content-Type",mime.lookup(url)+";charset=utf-8");
-        fs.readFile(pathname.slice(1),"utf-8",function(err,data){
-            res.write(data);
-            res.end();
-        })
+    if(pathname != '/favicon.ico'){
+        if( pathname=="/"){  //请求地址： http://localhost:8888
+            res.setHeader("Content-Type","text/html;charset=utf-8");
+            fs.readFile("index.html","utf-8",function(err,data){
+                res.write(data);
+                res.end();
+            })
+        }else{
+            res.setHeader("Content-Type",mime.getType(pathname)+";charset=utf-8"); // mime 2版本 将lookup 改为getType
+            console.log(mime.getType(pathname));
+            fs.readFile(pathname.slice(1),"utf-8",function(err,data){
+                res.write(data);
+                res.end();
+            })
+        }
     }
 
 }).listen(8888);
